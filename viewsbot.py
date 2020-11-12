1c5bc2812acc2ab8f74d00a6236a62a52e5691ec
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-12 17:08:09 -0600

    tested and working

--- viewsbot.py
+++ viewsbot.py
@@ -2,8 +2,10 @@
 import time
 from selenium import webdriver
 
+driver = webdriver.Chrome('[..]/chromedriver')
+
 # number of seconds to refresh browser
-Timer = 3 
+Timer = 10
 
 # url of video to view
 link = 'https://www.youtube.com/watch?v=wTcNtgA6gHs'
@@ -11,7 +13,6 @@ link = 'https://www.youtube.com/watch?v=wTcNtgA6gHs'
 # number of views to achieve
 views = 5
 
-driver = webdriver.Chrome()
 driver.get(link)
 
 for i in range(views):
